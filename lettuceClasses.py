9693f0e19faa2b2e6beea4041bb7bad22dc13f1f
Avery Brown <avery@averychristianbrown.com>
2016-04-04 19:03:47 -0400

    Update lettuceClasses.py
    Fixed an error in __Repr__ for SetPackage

--- lettuceClasses.py
+++ lettuceClasses.py
@@ -220,10 +220,10 @@ class SetPackage:
         self.name = set_name
 
     def __str__(self):
-        return self.set_name
+        return self.name
 
     def __repr__(self):
-        return self.set_name
+        return self.name
 
     # ---------------------------------------------------
     #                       Getters
